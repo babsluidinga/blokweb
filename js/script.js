764a6c3fbf4dc331e425b7db16dcf541583c82e7
babsluidinga <74204494+babsluidinga@users.noreply.github.com>
2021-02-23 20:00:47 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ var deKnop;
 deKnop = document.querySelector("header nav button");
 var hetMenu = document.querySelector("header nav section");
 var nav = document.querySelector("header nav");
+var svg = document.querySelector(".svgs");
 
 
 function openMenu() {
@@ -18,10 +19,15 @@ deKnop.addEventListener("click", openMenu);
 deKnop.addEventListener("click", veranderIcoon);
 
 
-//stack overflow heeft me bijgebracht over de onscroll function en pageYOffset... Dit omdat ik geen jQuery wilde gebruiken :) //
+//stack overflow heeft me bijgebracht over de onscroll function en pageYOffset... Dit omdat ik geen jQuery wilde gebruiken :) zelf de if in if bedacht though//
 window.onscroll = function () {
     if (window.pageYOffset > 10) {
-        nav.classList.add("solid");
+        nav.classList.add("solid")
+        if (window.pageYOffset > 1100) {
+            svg.classList.add("strokefill");
+        } else {
+            svg.classList.remove("strokefill")
+        }
     } else {
         nav.classList.remove("solid");
     }
